074e431fc28e10eb715b84ad7327e9e14c44e986
SajalSharma4 <sazalsharma2003@gmail.com>
2023-05-06 23:13:41 +0530

    main

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,15 +3,19 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import {
+  Navigate,
   createBrowserRouter,
 } from "react-router-dom";
+import Cookies from 'js-cookie';
+
+const token=Cookies.get('token');
 
 export default createBrowserRouter([
     {
       element: <App/>,
       children:[{
         path: "/",
-        element: <Home/>,
+        element: token!==undefined ? <Home/>:<Navigate to="/login" replace={true}/>,
       },
       {
         path: "/login",
